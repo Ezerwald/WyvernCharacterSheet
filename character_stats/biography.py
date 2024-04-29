daccfbfab9646c9fe8e80e1f57fc1b8b4d022836
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2024-04-29 12:35:15 +0300

    Create biography.py

---
+++ character_stats/biography.py
@@ -0,0 +1,42 @@
+from abstract_character import AbstractCharacter
+
+
+class Biography:
+    def __init__(self, character: AbstractCharacter, name: str = "", background: str = "", alignment: str = ""):
+        self.__character = character
+        self.name = name
+        self.background = background
+        self.alignment = alignment
+
+    @property
+    def name(self) -> str:
+        """Get the character's name."""
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        """Set the character's name."""
+        if name.strip():  # Ensure the name is not empty
+            self.__name = name.strip()
+        else:
+            raise ValueError("Name cannot be empty.")
+
+    @property
+    def background(self) -> str:
+        """Get the character's background."""
+        return self.__background
+
+    @background.setter
+    def background(self, background: str):
+        """Set the character's background."""
+        self.__background = background.strip()
+
+    @property
+    def alignment(self) -> str:
+        """Get the character's alignment."""
+        return self.__alignment
+
+    @alignment.setter
+    def alignment(self, alignment: str):
+        """Set the character's alignment."""
+        self.__alignment = alignment.strip()
