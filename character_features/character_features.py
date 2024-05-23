281168236c3657ad2af9d2dd0a128d4c2ff00703
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2024-05-23 18:38:58 +0300

    Modified features
    Features field now can show default features if input is ""

--- character_features/character_features.py
+++ character_features/character_features.py
@@ -4,20 +4,27 @@ from abstract_character import AbstractCharacter
 class CharacterFeatures:
     def __init__(self, character: AbstractCharacter, features: str = None):
         """Initializes CharacterFeatures object."""
-        self._character: AbstractCharacter = character
-        self._all_features = features or (
-            f"   {self._character.race.value.name} race features:\n"
-            f"{self._character.race.value.features}\n"
-            f"   {self._character.current_game_class.value.name} class features:\n"
-            f"{self._character.current_game_class.value.proficiencies}"
-        )
+        self.__character: AbstractCharacter = character
+        self.__all_features = features or self.get_default_features()
 
     @property
     def all_features(self) -> str:
         """Get all features."""
-        return self._all_features
+        return self.__all_features
 
     @all_features.setter
     def all_features(self, features: str):
         """Set all features."""
-        self._all_features = features
+        if features == "":
+            self.__all_features = self.get_default_features()
+        else:
+            self.__all_features = features
+
+    def get_default_features(self):
+        features = (
+            f"* {self.__character.race.value.name} race features:\n"
+            f"{self.__character.race.value.features}\n\n"
+            f"* {self.__character.current_game_class.value.name} class features:\n"
+            f"{self.__character.current_game_class.value.proficiencies}\n\n"
+        )
+        return features
