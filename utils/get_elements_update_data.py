ef3aa463d0cfc9d2e90e3e499f8b21dd05896d92
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2024-06-10 17:44:23 +0300

    No more sessions

--- utils/get_elements_update_data.py
+++ utils/get_elements_update_data.py
@@ -4,6 +4,7 @@ character_singleton = CharacterSingleton()
 
 
 def get_elements_update_data():
+    """Get all elements that should be updated and their new data."""
     data = {element: character_singleton.get_attribute(element)
             for element in character_singleton.get_input_id_to_object()}
     return data
